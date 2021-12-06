930921b271938d5dc3901a0123a7d4ed7f5d519b
gary <gary.white@lodgit.com>
2021-12-06 13:06:45 +0100

    boilerplate

---
+++ index.js
@@ -0,0 +1,9 @@
+function updateLight(current) {
+  
+    //your code here!
+  
+}
+
+console.log(updateLight("green")); // Return 'yellow'
+console.log(updateLight("yellow")); // Return 'red'
+console.log(updateLight("red")); // Return 'green'
